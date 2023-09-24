85d6c0cbf1d09f33140034e44effdd4da2b9a078
lmxx1234567 <lmxx1234567@hotmail.com>
2023-09-24 12:37:05 +0800

    feat: change model layers

--- model.py
+++ model.py
@@ -11,10 +11,11 @@ class ImageMerger(nn.Module):
         self.feature_extractor = nn.Sequential(*list(self.feature_extractor.children())[:-2])
         
         # Transposed convolution layers
-        self.deconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=8, stride=4, padding=2)
+        self.deconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1)
         self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1)
         self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
-        self.deconv4 = nn.ConvTranspose2d(128, 3, kernel_size=4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        self.deconv5 = nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1)
 
     def forward(self, img1, img2):
         x1 = self.feature_extractor(img1)
@@ -32,9 +33,14 @@ class ImageMerger(nn.Module):
         x = nn.ReLU()(self.deconv1(x))
         x = nn.ReLU()(self.deconv2(x))
         x = nn.ReLU()(self.deconv3(x))
-        x = torch.sigmoid(self.deconv4(x))
+        x = nn.ReLU()(self.deconv4(x))
+        x = torch.sigmoid(self.deconv5(x))
         
         return x
+    
+    def set_feature_extractor_grad(self, requires_grad):
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = requires_grad
 
 # if __name__ == "__main__":
 #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
