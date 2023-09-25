7afbcb571f7385b9cee72a2bbd001ca660cc5e12
lmxx1234567 <lmxx1234567@hotmail.com>
2023-09-25 20:56:26 +0800

    feat: add tag smoothing

--- train.py
+++ train.py
@@ -30,8 +30,8 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
         discriminator.set_feature_extractor_grad(frezze_feature_extractor)
 
     criterion = nn.BCELoss()
-    optimizer_gen = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    optimizer_dis = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    optimizer_gen = optim.Adam(generator.parameters(), lr=0.0001, betas=(0.5, 0.999))
+    optimizer_dis = optim.Adam(discriminator.parameters(), lr=0.000001, betas=(0.5, 0.999))
 
     # load generator
     generator,optimizer_gen,start_epoch_gen = load_generator(checkpoint_path=checkpoint_path_gen,model=generator,optimizer=optimizer_gen)
@@ -47,7 +47,7 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     # Define hyperparameters
-    num_epochs = 10
+    num_epochs = 20
     print_every = 100  # Print training stats every 100 batches
 
     for epoch in range(start_epoch, num_epochs):
@@ -55,6 +55,7 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
         pbar = tqdm(total=len(dataloader))
         running_loss_gen = 0.0
         running_loss_dis = 0.0
+        running_acc = 0.0
         for i,(left_img, right_img, real_merged_img) in enumerate(dataloader):  # Assume dataloader is created from your EmojiDataset
             # Move data to device
             left_img, right_img, real_merged_img = (
@@ -74,8 +75,8 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
             real_labels = torch.ones((logits_real.size(0), 1)).to(device)
             fake_labels = torch.zeros((logits_fake.size(0), 1)).to(device)
             
-            loss_real = criterion(logits_real, real_labels)
-            loss_fake = criterion(logits_fake, fake_labels)
+            loss_real = criterion(logits_real, real_labels*0.9)
+            loss_fake = criterion(logits_fake, fake_labels+0.1)
             
             loss_dis = (loss_real + loss_fake) / 2
             loss_dis.backward()
@@ -85,7 +86,7 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
             optimizer_gen.zero_grad()
             
             logits_fake = discriminator(left_img, right_img, fake_merged_img)
-            loss_gen = criterion(logits_fake, real_labels)  # We want the generator to fool the discriminator
+            loss_gen = criterion(logits_fake, real_labels*0.9)  # We want the generator to fool the discriminator
             
             loss_gen.backward()
             optimizer_gen.step()
@@ -94,11 +95,13 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
             # Print statistics
             running_loss_dis += loss_dis.item()
             running_loss_gen += loss_gen.item()
+            running_acc += torch.sum((logits_fake > 0.5) == real_labels).item()
             if i % print_every == 0:
                 running_loss_dis = 0.0
                 running_loss_gen = 0.0
+                running_acc = 0.0
             else:
-                pbar.set_postfix(loss_gen=running_loss_gen / (i%print_every),loss_dis=running_loss_dis / (i%print_every))
+                pbar.set_postfix(loss_gen=running_loss_gen / (i%print_every),loss_dis=running_loss_dis / (i%print_every), acc=running_acc/(i%print_every*batch_size))
             pbar.update(1)
             pbar.set_description(f"Epoch [{epoch + 1}/{num_epochs}]")
 
@@ -107,7 +110,6 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
         save_discriminator(checkpoint_path=checkpoint_path_dis,epoch=epoch,model=discriminator,optimizer=optimizer_dis)
 
         pbar.close()
-        break
 
     print("Finished Training")
 
