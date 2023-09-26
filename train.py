29f04c2f51353a46eebaccebeeddb8a8eb8e25db
lmxx1234567 <lmxx1234567@hotmail.com>
2023-09-26 16:38:50 +0800

    feat: add conditional update

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 import argparse
 from generator_train import load_generator,save_generator
 from discriminator_train import load_discriminator,save_discriminator
+from collections import deque
+
 
 def initialize_weights(layer):
     if isinstance(layer, nn.ConvTranspose2d):
@@ -30,8 +32,8 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
         discriminator.set_feature_extractor_grad(frezze_feature_extractor)
 
     criterion = nn.BCELoss()
-    optimizer_gen = optim.Adam(generator.parameters(), lr=0.0001, betas=(0.5, 0.999))
-    optimizer_dis = optim.Adam(discriminator.parameters(), lr=0.000001, betas=(0.5, 0.999))
+    optimizer_gen = optim.Adam(generator.parameters(), lr=1e-4, betas=(0.5, 0.999))
+    optimizer_dis = optim.Adam(discriminator.parameters(), lr=1e-5, betas=(0.5, 0.999))
 
     # load generator
     generator,optimizer_gen,start_epoch_gen = load_generator(checkpoint_path=checkpoint_path_gen,model=generator,optimizer=optimizer_gen)
@@ -48,14 +50,18 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
 
     # Define hyperparameters
     num_epochs = 20
-    print_every = 100  # Print training stats every 100 batches
+
+    # Define the length of the sliding window
+    window_length = 100
+
+    # Initialize deques
+    loss_gen_window = deque(maxlen=window_length)
+    loss_dis_window = deque(maxlen=window_length)
+    acc_window = deque(maxlen=window_length)
 
     for epoch in range(start_epoch, num_epochs):
         # Init progess bar
         pbar = tqdm(total=len(dataloader))
-        running_loss_gen = 0.0
-        running_loss_dis = 0.0
-        running_acc = 0.0
         for i,(left_img, right_img, real_merged_img) in enumerate(dataloader):  # Assume dataloader is created from your EmojiDataset
             # Move data to device
             left_img, right_img, real_merged_img = (
@@ -64,44 +70,50 @@ def train(checkpoint_path_gen=None,checkpoint_path_dis=None,frezze_feature_extra
                 real_merged_img.to(device),
             )
 
-            # Train discriminator
-            optimizer_dis.zero_grad()
-            
+            # Generate fake merged image
             fake_merged_img = generator(left_img, right_img)
-            
-            logits_real = discriminator(left_img, right_img, real_merged_img)
-            logits_fake = discriminator(left_img, right_img, fake_merged_img.detach())
 
-            real_labels = torch.ones((logits_real.size(0), 1)).to(device)
-            fake_labels = torch.zeros((logits_fake.size(0), 1)).to(device)
-            
-            loss_real = criterion(logits_real, real_labels*0.9)
-            loss_fake = criterion(logits_fake, fake_labels+0.1)
-            
-            loss_dis = (loss_real + loss_fake) / 2
-            loss_dis.backward()
-            optimizer_dis.step()
-            
+            # Initialize labels
+            real_labels = torch.ones((fake_merged_img.size(0), 1)).to(device)
+            fake_labels = torch.zeros((fake_merged_img.size(0), 1)).to(device)
+
             # Train generator
             optimizer_gen.zero_grad()
             
             logits_fake = discriminator(left_img, right_img, fake_merged_img)
-            loss_gen = criterion(logits_fake, real_labels*0.9)  # We want the generator to fool the discriminator
+            loss_gen = criterion(logits_fake, real_labels)  # We want the generator to fool the discriminator
             
             loss_gen.backward()
             optimizer_gen.step()
+            
+            # Train discriminator
+            optimizer_dis.zero_grad()
+            
+            logits_real = discriminator(left_img, right_img, real_merged_img)
+            logits_fake = discriminator(left_img, right_img, fake_merged_img.detach())
+            
+            loss_real = criterion(logits_real, real_labels)
+            loss_fake = criterion(logits_fake, fake_labels)
+            
+            loss_dis = (loss_real + loss_fake) / 2
 
+            # Append current loss and accuracy values to deques
+            loss_gen_window.append(loss_gen.item())
+            loss_dis_window.append(loss_dis.item())
+            acc_window.append(torch.sum((logits_fake > 0.5) == real_labels).item())
+            
+            # Compute averages over the sliding window
+            avg_loss_gen = sum(loss_gen_window) / len(loss_gen_window)
+            avg_loss_dis = sum(loss_dis_window) / len(loss_dis_window)
+            avg_acc = sum(acc_window) / (len(acc_window) * batch_size)
 
-            # Print statistics
-            running_loss_dis += loss_dis.item()
-            running_loss_gen += loss_gen.item()
-            running_acc += torch.sum((logits_fake > 0.5) == real_labels).item()
-            if i % print_every == 0:
-                running_loss_dis = 0.0
-                running_loss_gen = 0.0
-                running_acc = 0.0
-            else:
-                pbar.set_postfix(loss_gen=running_loss_gen / (i%print_every),loss_dis=running_loss_dis / (i%print_every), acc=running_acc/(i%print_every*batch_size))
+            # Conditionally update discriminator
+            if avg_acc > 0.6:
+                loss_dis.backward()
+                optimizer_dis.step()
+            
+            # Update the progress bar
+            pbar.set_postfix(loss_gen=avg_loss_gen, loss_dis=avg_loss_dis, acc=avg_acc)
             pbar.update(1)
             pbar.set_description(f"Epoch [{epoch + 1}/{num_epochs}]")
 
